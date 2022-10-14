09d6076189a2eee8e791f3cf18560185639b4237
Wesley Teal <wtee@users.noreply.github.com>
2022-10-14 14:50:10 -0500

    Handle AViary_to_Islandora sheets; Can output arkimedes TSV instead of minting.

--- src/gshark/__main__.py
+++ src/gshark/__main__.py
@@ -5,7 +5,7 @@ from gshark.gshark import (
     get_aviary_metadata,
     get_islandora_metadata,
     mint_arks,
-    save_google_sheet
+    save_tsv
 )
 
 
@@ -17,12 +17,21 @@ def main():
     parser.add_argument("source")
     parser.add_argument("outfile")
     parser.add_argument("platform")
+    parser.add_argument("--tsv-only", action="store_true")
     args = parser.parse_args()
 
     gs_md = load_google_sheet(args.source)
-    arks_md = get_islandora_metadata(gs_md) if args.platform == "islandora" else get_aviary_metadata(gs_md)
-    arks = mint_arks(arks_md, args.username, args.password, args.shoulder)
-    save_google_sheet(gs_md, arks, args.outfile, args.platform)
+    if args.platform == "islandora":
+        arks_md = get_islandora_metadata(gs_md)
+    elif args.platform == "aviary":
+        arks_md = get_aviary_metadata(gs_md)
+    elif args.platform == "aviary-to-islandora":
+        arks_md = get_aviary_metadata(gs_md, True)
+    if not args.tsv_only:
+        arks = mint_arks(arks_md, args.username, args.password, args.shoulder)
+        save_tsv(gs_md, args.outfile, args.platform, arks)
+    else:
+        save_tsv(gs_md, args.outfile, "arkimedes")
 
 
 if __name__ == "__main__":
--- src/gshark/gshark.py
+++ src/gshark/gshark.py
@@ -1,5 +1,4 @@
 """Devouring Google Sheets and making baby ARKs doo doo, doo doo doo doo"""
-import argparse
 import csv
 
 from arkimedes.ezid import build_anvl, upload_anvl
@@ -15,7 +14,33 @@ def load_google_sheet(gs_file, delimiter="\t", quotechar=None):
         return list(reader)
 
 
-def get_aviary_metadata(gs_md):
+def get_aviary_metadata(gs_md, aviary_to_islandora=False):
+    if aviary_to_islandora:
+        ark_md = [
+            {
+                "dc.creator": "; ".join(
+                    [
+                        g.get("interviewee", ""),
+                        g.get("interviewer", ""),
+                        g.get("personal_creator", ""),
+                        g.get("corporate_creator", "")
+                    ]
+                ).strip("; "),
+                "dc.title": g.get("title", ""),
+                "dc.date": g.get("date_original", ""),
+                "_target": g.get("ark", ""),
+                "dc.type": g.get("dcmi_type", "")
+            }
+            if g.get("parent_predicate") == "isMemberOfCollection"
+            else ""
+            for g
+            in gs_md
+        ]
+        # The Islandora digital collection will usually lack a URL, so
+        #
+        if not ark_md[0]["_target"].startswith("http"):
+            arks_md[0]["_target"] = f"https://digitalcollections.lib.iastate.edu/islandora/object/{gs_md[0]['pid']}"
+
     return [
         {
             "dc.creator": "; ".join(
@@ -92,14 +117,18 @@ def mint_arks(md, user_name, password, shoulder):
     ]
 
 
-def save_google_sheet(gs_md, arks, out_file, platform, dialect=csv.excel_tab):
-    ark_column = "ark" if platform == "islandora" else "ARK"
-    for g, a in zip(gs_md, arks):
-        g[ark_column] = a
+def save_tsv(md, out_file, out_type, arks=None, dialect=csv.excel_tab):
+    if out_type != "arkimedes":
+        ark_column = "ark"
+        if out_type == "aviary":
+            ark_column = ark_column.upper()
+
+        for g, a in zip(md, arks):
+            g[ark_column] = a
 
     with open(out_file, "w", encoding="utf8", newline="") as fh:
-        fieldnames = list(gs_md[0].keys())
+        fieldnames = list(md[0].keys())
         writer = csv.DictWriter(fh, fieldnames=fieldnames, dialect=dialect)
 
         writer.writeheader()
-        writer.writerows(gs_md)
+        writer.writerows(md)
